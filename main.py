d52ac2cf6e5cb272f2ebaf3d0f5e0548d24334cf
globalfranck <50407517+globalfranck@users.noreply.github.com>
2021-02-11 19:13:58 +0100

    Bank text logo + adding menu

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # Bank project
 # Creating a menu: new customer, existing customer and exit the program
+
+bank_logo = " -------------------- \n --- Bank and Co. --- \n -------------------- \n "
+menu = "1) New customer \n 2) Existing customer \n 3) Exit program \n Enter your choice : "
+user_choice=int(input(bank_logo + menu))
